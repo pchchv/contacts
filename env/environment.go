e63551c25ae6937ced30821e4249630b4fc90154
pchchv <ipchchv@gmail.com>
2024-10-08 11:51:10 +0300

    Refactoring

---
+++ env/environment.go
@@ -0,0 +1,25 @@
+package env
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// Load values from .env into the system.
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value.
+	// Outputs a panic if the value is missing.
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panic("Value " + v + "does not exist")
+	}
+	return value
+}
