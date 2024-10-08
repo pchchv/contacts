058f70c7ebda3ab3527a354c10601d69307ad289
pchchv <ipchchv@gmail.com>
2024-10-08 12:28:26 +0300

    Implement GetContact and GetContacts funcs

--- models/contact.go
+++ models/contact.go
@@ -1,10 +1,33 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Contact struct {
 	gorm.Model
 	Name   string `json:"name"`
 	Phone  string `json:"phone"`
 	UserId uint   `json:"user_id"` // user to whom the contact belongs
+}
+
+func GetContact(id uint) *Contact {
+	contact := &Contact{}
+	if err := GetDB().Table("contacts").Where("id = ?", id).First(contact).Error; err != nil {
+		return nil
+	}
+
+	return contact
+}
+
+func GetContacts(user uint) []*Contact {
+	contacts := make([]*Contact, 0)
+	if err := GetDB().Table("contacts").Where("user_id = ?", user).Find(&contacts).Error; err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return contacts
 }
