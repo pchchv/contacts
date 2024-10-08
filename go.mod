466ee3f6f8b2e02e3cf3c8c552969a98447b27e3
pchchv <ipchchv@gmail.com>
2024-10-08 12:10:13 +0300

    Implement a Account structure

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.21.5
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/jinzhu/inflection v1.0.0 // indirect
--- models/accounts.go
+++ models/accounts.go
@@ -1,9 +1,19 @@
 package models
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/jinzhu/gorm"
+)
 
-// JWT access rights structure.
+// JWT access rights.
 type Token struct {
 	UserId uint
 	jwt.StandardClaims
+}
+
+type Account struct {
+	gorm.Model
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Token    string `json:"token";sql:"-"`
 }
