d5e28372ded38c22516894fae493f0d8ac0d7324
pchchv <ipchchv@gmail.com>
2024-10-08 12:29:57 +0300

    Implement a method Validate

--- models/contact.go
+++ models/contact.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
+	u "github.com/pchchv/contacts/utils"
 )
 
 type Contact struct {
@@ -11,6 +12,24 @@ type Contact struct {
 	Name   string `json:"name"`
 	Phone  string `json:"phone"`
 	UserId uint   `json:"user_id"` // user to whom the contact belongs
+}
+
+// Checks the mandatory parameters sent through the body of the http request.
+// Returns a message and true if the requirement is met.
+func (contact *Contact) Validate() (map[string]interface{}, bool) {
+	if contact.Name == "" {
+		return u.Message(false, "Contact name should be on the payload"), false
+	}
+
+	if contact.Phone == "" {
+		return u.Message(false, "Phone number should be on the payload"), false
+	}
+
+	if contact.UserId <= 0 {
+		return u.Message(false, "User is not recognized"), false
+	}
+
+	return u.Message(true, "success"), true
 }
 
 func GetContact(id uint) *Contact {
