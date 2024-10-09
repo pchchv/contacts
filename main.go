cb004962f6105516f30403e9559764a68ddc55e5
pchchv <ipchchv@gmail.com>
2024-10-09 13:54:51 +0300

    Implement app runner

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/pchchv/contacts/app"
+	"github.com/pchchv/contacts/controllers"
+)
+
 func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET")
+	// attach JWT auth middleware
+	router.Use(app.JwtAuthentication)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	log.Println(port)
+
+	// launch the app, visit localhost:8000/api
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Println(err)
+	}
 }
