18833855857f4b5273776e113274dc9b969f5336
pchchv <ipchchv@gmail.com>
2024-10-09 13:49:21 +0300

    Implement a function Authenticate

--- controllers/authController.go
+++ controllers/authController.go
@@ -19,3 +19,15 @@ func CreateAccount(w http.ResponseWriter, r *http.Request) {
 	resp := account.Create()
 	u.Respond(w, resp)
 }
+
+func Authenticate(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+	// decode the request body into struct and failed if any error occur
+	if err := json.NewDecoder(r.Body).Decode(account); err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := models.Login(account.Email, account.Password)
+	u.Respond(w, resp)
+}
